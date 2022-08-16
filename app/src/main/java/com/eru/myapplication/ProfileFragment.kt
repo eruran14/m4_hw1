d5cd35b6a65e4e9efb4e9853babdf0421a6bcfc6
eruran14 <eruran14@gmail.com>
2022-08-16 09:26:24 +0600

    Initial commit

---
+++ app/src/main/java/com/eru/myapplication/ProfileFragment.kt
@@ -0,0 +1,41 @@
+package com.eru.myapplication
+
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eru.myapplication.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,3)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && data != null){
+            val selectedImage: Uri? = data.data
+            binding.ivImage.setImageURI(selectedImage)
+        }
+    }
+}
